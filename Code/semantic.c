e4c225a6ab4b713e4aa4c375888888fc09f455b1
pkun <181250068@smail.nju.edu.cn>
2021-04-07 21:06:33 +0800

    add skeleton

--- Code/semantic.c
+++ Code/semantic.c
@@ -7,18 +7,58 @@ int programAnalyze(int last_node){
         // printf("%s %s %s\n", child.name, child.value, node.name);
         ExtDefListAnalyze(node.child);
     }while(child.brother!=-1);
+    return 0;
 }
 
 
 int ExtDefListAnalyze(int last_node){
     ASTNode ExtDefList = nodes[last_node];
     //type = 1 represent ExtDefList -> ExtDef ExtDefList
-    if(ExtDefList.type==1){
-        
-        ExtDefListAnalyze
+    if(ExtDefList.type==ExtDefList_ExtDefExtDefList){
+        int ExtDef = ExtDefList.child;
+        int ExtDefList = nodes[ExtDef].brother;
+        ExtDefAnalyze(ExtDef);
+        ExtDefListAnalyze(ExtDefList);
     }
+    else{
+
+    }
+    return 0;
+}
+
+int ExtDefAnalyze(int ExtDef){
+    ASTNode ExtDefNode = nodes[ExtDef];
+    int child = ExtDefNode.child;
+    ASTNode childNode = nodes[child];
+    switch (ExtDefNode.type)
+    {
+    case ExtDef_SpecifierExtDecListSEMI:
+        break;
+    case ExtDef_SpecifierSEMI:
+        break;
+    case ExtDef_SpecifierFunDecSEMI:
+        SpecifierAnalyze(child);
+        FunDecAnalyze(childNode.brother);
+        // childNode = nodes[childNode.brother];
+        break;
+    case ExtDef_SpecifierFunDecCompSt:
+        break;
+    default:
+        break;
+    }
+    printf("%s %d\n", ExtDefNode.name, ExtDefNode.type);
+}
+
+int SpecifierAnalyze(int Specifier){
+    ASTNode spe = nodes[Specifier];
+    printf("%s %d\n", spe.name, spe.type);
+}
+
+int FunDecAnalyze(int funDec){
+    ASTNode fun = nodes[funDec];
+    printf("%s %d\n", fun.name, fun.type);
 }
 
 int semanticAnalyze(int last_node){
-    programAnalyze(last_node);
+    return programAnalyze(last_node);
 }
--- Code/semantic.h
+++ Code/semantic.h
@@ -1,5 +1,23 @@
 #include "global.h"
 
+#define ExtDefList_ExtDefExtDefList 1
+#define ExtDef_SpecifierExtDecListSEMI 1
+#define ExtDef_SpecifierSEMI 2
+#define ExtDef_SpecifierFunDecSEMI 3
+#define ExtDef_SpecifierFunDecCompSt 4
+#define ExtDecList_VarDec 1
+#define ExtDecList_VarDecCOMMAExtDecList 2
+#define Specifier_TYPE 1
+#define Specifier_StructSpecifier 2
+#define StructSpecifier_STRUCTOptTagLCDefListRC 1
+#define StructSpecifier_STRUCTTag 2
+#define OptTag_ID 1
+#define Tag_ID 1
+#define VarDec_ID 1
+#define VarDec_LBINTRB 2
+#define FunDec_IDLPVarListRP 1
+#define FunDec_IDLPRP 2
+
 int semanticAnalyze(int);
 int programAnalyze(int);
 int ExtDefListAnalyze(int);

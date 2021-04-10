c9ae025a80d197d894b0efd2a042ce4fe15c8a40
pkun <181250068@smail.nju.edu.cn>
2021-04-11 00:28:52 +0800

    almost done analyst struct and add debug,hash calculate and some data structure

--- Code/semantic.c
+++ Code/semantic.c
@@ -1,64 +1,182 @@
 #include "semantic.h"
 
-int programAnalyze(int last_node){
-    ASTNode node = nodes[last_node];
-    ASTNode child = nodes[node.child];
-    do{
+void DebugPrintNameType(ASTNode node)
+{
+    if (SEMANTIC_DEBUG)
+        printf("%s %d\n", node.name, node.type);
+}
+
+void DebugPrintNameValue(ASTNode node)
+{
+    if (SEMANTIC_DEBUG)
+        printf("%s %s\n", node.name, node.value);
+}
+
+int *GetSon(ASTNode node)
+{
+    int *sons = malloc(sizeof(int) * 10);
+    int index = 0;
+    sons[index++] = node.child;
+    ASTNode child = nodes[sons[index - 1]];
+    while (child.brother != -1)
+    {
+        sons[index++] = child.brother;
+        child = nodes[sons[index - 1]];
+    }
+    // sons[index++] = nodes[node.child];
+    // ASTNode child = sons[index-1];
+    // while(child.brother!=-1){
+    //     sons[index++] = nodes[child.brother];
+    //     child = sons[index-1];
+    // }
+    return sons;
+}
+
+int SymbolHash(Symbol symbol)
+{
+    
+}
+
+unsignedinthash_pjw(char *name)
+{
+    unsigned int val = 0, i;
+    for (; *name; ++name)
+    {
+        val = (val << 2) + *name;
+        if (i = val & ~0x3fff)
+            val = (val ^ (i >> 12)) & 0x3fff;
+    }
+    return val;
+}
+
+int ProgramAnalyze(int index)
+{
+    ASTNode program = nodes[index];
+    DebugPrintNameType(program);
+    ASTNode child = nodes[program.child];
+    do
+    {
         // printf("%s %s %s\n", child.name, child.value, node.name);
-        ExtDefListAnalyze(node.child);
-    }while(child.brother!=-1);
+        ExtDefListAnalyze(program.child);
+    } while (child.brother != -1);
     return 0;
 }
 
-
-int ExtDefListAnalyze(int last_node){
-    ASTNode ExtDefList = nodes[last_node];
+int ExtDefListAnalyze(int index)
+{
+    ASTNode ext_def_list = nodes[index];
     //type = 1 represent ExtDefList -> ExtDef ExtDefList
-    if(ExtDefList.type==ExtDefList_ExtDefExtDefList){
-        int ExtDef = ExtDefList.child;
-        int ExtDefList = nodes[ExtDef].brother;
-        ExtDefAnalyze(ExtDef);
-        ExtDefListAnalyze(ExtDefList);
+    if (ext_def_list.type == ExtDefList_ExtDefExtDefList)
+    {
+        int ext_def = ext_def_list.child;
+        int ext_def_list = nodes[ext_def].brother;
+        ExtDefAnalyze(ext_def);
+        ExtDefListAnalyze(ext_def_list);
     }
-    else{
-
+    else
+    {
     }
     return 0;
 }
 
-int ExtDefAnalyze(int ExtDef){
-    ASTNode ExtDefNode = nodes[ExtDef];
-    int child = ExtDefNode.child;
-    ASTNode childNode = nodes[child];
-    switch (ExtDefNode.type)
+int ExtDefAnalyze(int index)
+{
+    ASTNode ext_def_node = nodes[index];
+    DebugPrintNameType(ext_def_node);
+    int child = ext_def_node.child;
+    ASTNode child_node = nodes[child];
+    switch (ext_def_node.type)
     {
     case ExtDef_SpecifierExtDecListSEMI:
         break;
     case ExtDef_SpecifierSEMI:
+        SpecifierAnalyze(child);
         break;
     case ExtDef_SpecifierFunDecSEMI:
-        SpecifierAnalyze(child);
-        FunDecAnalyze(childNode.brother);
-        // childNode = nodes[childNode.brother];
+    {
+        Type type = SpecifierAnalyze(child);
+        type.value = RIGHT;
+        FunDecAnalyze(child_node.brother);
         break;
+    }
+        // childNode = nodes[childNode.brother];
     case ExtDef_SpecifierFunDecCompSt:
         break;
     default:
         break;
     }
-    printf("%s %d\n", ExtDefNode.name, ExtDefNode.type);
 }
 
-int SpecifierAnalyze(int Specifier){
-    ASTNode spe = nodes[Specifier];
-    printf("%s %d\n", spe.name, spe.type);
+Type SpecifierAnalyze(int index)
+{
+    ASTNode specifier = nodes[index];
+    DebugPrintNameType(specifier);
+    printf("%s\n", "struct analyze");
+    ASTNode spe_child = nodes[specifier.child];
+    switch (specifier.type)
+    {
+    case Specifier_TYPE:
+    {
+        Type type;
+        type.kind = BASIC;
+        type.value = LEFT;
+        int res = strcmp(spe_child.value, "int");
+        if (res == 0)
+            type.basic = INT;
+        else
+            type.basic = FLOAT;
+        return type;
+    }
+    case Specifier_StructSpecifier:
+    {
+        StructAnalyze(specifier.child);
+        break;
+    }
+
+    default:
+        break;
+    }
 }
 
-int FunDecAnalyze(int funDec){
-    ASTNode fun = nodes[funDec];
-    printf("%s %d\n", fun.name, fun.type);
+int StructAnalyze(int index)
+{
+    ASTNode struct_ = nodes[index];
+    DebugPrintNameType(struct_);
+    int *sons = GetSon(struct_);
+    Type type;
+    type.kind = STRUCTURE;
+    type.value = LEFT;
+    Field field;
+    char *name = OptTagAnalyze(sons[1]);
+    Symbol symbol;
+    symbol.name = name;
+    symbol.kind = STRUCTURE;
+    symbol.type = type;
+    DebugPrintNameValue(nodes[sons[0]]);
+    DebugPrintNameType(nodes[sons[1]]);
+    DebugPrintNameValue(nodes[sons[2]]);
+    DebugPrintNameType(nodes[sons[3]]);
+    DebugPrintNameValue(nodes[sons[4]]);
 }
 
-int semanticAnalyze(int last_node){
-    return programAnalyze(last_node);
+char *OptTagAnalyze(int index)
+{
+    ASTNode opt_tag = nodes[index];
+    switch (opt_tag.type)
+    {
+    case OptTag_ID:
+        return nodes[opt_tag.child].value;
+    default:
+        return ANONYMITY;
+    }
+}
+
+int FunDecAnalyze(int index)
+{
+    ASTNode fun_dec = nodes[index];
+}
+
+int semanticAnalyze(int last_node)
+{
+    return ProgramAnalyze(last_node);
 }
--- Code/semantic.h
+++ Code/semantic.h
@@ -1,5 +1,6 @@
 #include "global.h"
 
+#define SEMANTIC_DEBUG 1
 #define ExtDefList_ExtDefExtDefList 1
 #define ExtDef_SpecifierExtDecListSEMI 1
 #define ExtDef_SpecifierSEMI 2
@@ -17,18 +18,76 @@
 #define VarDec_LBINTRB 2
 #define FunDec_IDLPVarListRP 1
 #define FunDec_IDLPRP 2
+#define ANONYMITY "!@#$"
+
+typedef struct Type Type;
+typedef struct Field Field;
+typedef struct Symbol Symbol;
+typedef enum TypeKind TypeKind;
+typedef enum TypeValue TypeValue;
+typedef enum BasicType BasicType;
+typedef enum SymbolKind SymbolKind;
+
+enum TypeKind {
+    BASIC,ARRAY,STRUCTURE
+};
+
+enum TypeValue{
+    LEFT,RIGHT
+};
+
+enum BasicType{
+    INT,FLOAT
+};
+
+struct Type{
+    TypeKind kind;
+    TypeValue value;
+    union{
+        BasicType basic;
+        struct{
+            Type* type;
+            int size;
+        } array;
+        Field* field;
+    };
+};
+
+struct Field{
+    Type* type;
+    char* name;
+    Field* next;
+};
+
+enum SymbolKind {
+    VAR,
+    FIELD,
+    STRUCTURE,
+    FUNCION
+};
+
+struct Symbol
+{
+    SymbolKind kind;
+    char* name;
+    Type type;
+    int hashcode;
+    Symbol* next;
+};
+
+Symbol* symbolTable;
 
 int semanticAnalyze(int);
-int programAnalyze(int);
+int ProgramAnalyze(int);
 int ExtDefListAnalyze(int);
 int ExtDefAnalyze(int);
-int SpecifierAnalyze(int);
+Type SpecifierAnalyze(int);
 int ExtDecListAnalyze(int);
 int FunDecAnalyze(int);
 int CompStAnalyze(int);
 int VarDecAnalyze(int);
 int StructAnalyze(int);
-int OptTagAnalyze(int);
+char* OptTagAnalyze(int);
 int DefListAnalyze(int);
 int TagAnalyze(int);
 int VarListAnalyze(int);
